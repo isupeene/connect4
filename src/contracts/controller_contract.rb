2873bb0ba61d5a15d75a4e4e829f65df5049122a
Isaac Supeene <isupeene@ualberta.ca>
2014-11-12 14:27:03 -0700

    Corrected the controller contract to account for the actual nature of a connect 4 board.

--- src/contracts/controller_contract.rb
+++ src/contracts/controller_contract.rb
@@ -1,17 +1,17 @@
 require 'test/unit'
 
 module ControllerContract
-	def play_precondition(i, j)
+	def play_precondition(column)
 		assert(my_turn, "You can only move on your turn")
 		assert(
-			game.valid_move(i, j),
-			"You can only place a token in a valid space."
+			game.valid_move(column),
+			"You can only place a token in a valid column with room remaining."
 		)
 	end
 	
-	def play_postcondition(i, j, result)
+	def play_postcondition(column, result)
 		assert_equal(
-			player_number, game.board[i, j],
+			player_number, game.board[-board.number_of_tokens(column), column],
 			"After placing a token, the game board will be " \
 			"(internally) marked with the player number."
 		)
