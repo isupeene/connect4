f306e611457118710eaea484d30ad2b0c71d9463
Isaac Supeene <isupeene@ualberta.ca>
2014-11-12 19:07:20 -0700

    Added options for single-player.

--- src/contracts/game_manager_contract.rb
+++ src/contracts/game_manager_contract.rb
@@ -2,12 +2,21 @@ require 'test/unit'
 require_relative 'controller_contract'
 
 module GameManagerContract
-	def start_game_postcondition(game_type, result)
-		assert(
-			result.is_a?(ControllerContract),
-			"Starting a new game returns the human player's controller."
-		)
-		assert(game_in_progress)
+	def start_game_postcondition(game_options, result)
+		if game_options.single_player?
+			assert(
+				result.is_a?(ControllerContract),
+				"Starting a new game returns the human player's controller."
+			)
+			assert(game_in_progress)
+		else
+			assert_equals(2, result.length, "Two controllers are returned.")
+			assert(result.all?{ |c| c.is_a?(ControllerContract) })
+			
+			controller_1, controller_2 = *result
+			assert_equals(1, controller_1.player_number)
+			assert_equals(2, controller_2.player_number)
+		end
 	end
 	
 	def save_game_precondition
