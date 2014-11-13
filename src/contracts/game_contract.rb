01c315713f6bbc14d6da425c5704195d2e55ab2b
Isaac Supeene <isupeene@ualberta.ca>
2014-11-12 18:58:56 -0700

    Added contracts for the size of the board, specified the fill property, and added a contract for adding a single token.

--- src/contracts/game_contract.rb
+++ src/contracts/game_contract.rb
@@ -4,9 +4,21 @@ require_relative 'controller_contract'
 module GameContract
 	def invariant
 		assert(
-			board.all{ |t| t.nil? || 1..2 === t },
+			board.each_with_index.all? { |t, i, j|
+				(i < board.height - board.number_of_tokens(j) && t.nil?) ||
+				(i >= board.height - board.number_of_tokens(j) && 1..2 === t)
+			},
 			"Each position on the board is either empty, " \
-			"or contains a valid player token."
+			"or contains a valid player token.\n" \
+			"Columns are filled from the bottom up."
+		)
+		assert_equals(
+			7, board.width,
+			"The game board has a width of 7."
+		)
+		assert_equals(
+			6, board.height,
+			"The game board has a height of 6."
 		)
 	end
 
@@ -38,5 +50,11 @@ module GameContract
 		)
 		
 		# TODO: Somehow assert that a turn update or a game over message has been sent to all views.
+	end
+	
+	def play_invariant(column, player_number, result)
+		previous_number_of_tokens = board.number_of_tokens(column)
+		yield
+		assert_equals(previous_number_of_tokens + 1, board.number_of_tokens(column))
 	end
 end
