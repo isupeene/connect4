aef97ca8be4dc20a7a20afce6f84f5599829c3a9
Isaac Supeene <isupeene@ualberta.ca>
2014-11-17 15:58:51 -0700

    Fixed wrap-around victory conditions, and added a tie-game condition when the board is full.

--- src/implementation/victory_conditions.rb
+++ src/implementation/victory_conditions.rb
@@ -4,11 +4,19 @@ def connect4_victory_condition(board)
 
 		[[-1, -1], [-1,  0], [-1, 1], [0, -1],
 		 [0 ,  1], [ 1, -1], [ 1, 0], [1,  1]].each { |p, q|
-			if [1, 2, 3].all?{ |m| board[i + m*p, j + m*q] == x }
+			if [1, 2, 3].all?{ |m|
+				(0...board.height) === i + m*p &&
+				(0...board.width) === j + m*q &&
+				board[i + m*p, j + m*q] == x
+			}
 				return x
 			end
 		}
 	}
-	return nil
+	if (0...board.width).all?{|i| board.number_of_tokens(i) == board.height }
+		return 0
+	else
+		return nil
+	end
 end
 
