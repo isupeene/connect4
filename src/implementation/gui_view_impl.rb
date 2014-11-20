a7db3428bb457c1a4ffd74469f4982e1fb26a608
bsoetaer <bsoetaer@ualberta.ca>
2014-11-19 19:56:53 -0800

    Commiting to pull in master changes

--- src/implementation/gui_view_impl.rb
+++ src/implementation/gui_view_impl.rb
@@ -2,20 +2,23 @@
 require 'gtk2'
 #TODO No impl
 require_relative "game_manager_impl"
+require_relative 'gui_connect4_single_player_view_impl'
+require_relative 'gui_connect4_multiplayer_view_impl'
+require_relative 'gui_otto_and_toot_single_player_view_impl'
+require_relative 'gui_otto_and_toot_multiplayer_view_impl'
 
 class GUIViewImpl
 
-	@@white = "#FFFFFF"
-	@@black = "#000000"
-	@@red = "#FF0000"
+	@@white = Gdk::Color.parse("#FFFFFF")
 	@@game_types = {1 => "Connect 4", 2 => "Otto and Toot"}
+	@buttons = Array.new(6){Array.new(7)}
 
 	def initialize
 		if __FILE__ == $0
 			Gtk.init
 	
 			@builder = Gtk::Builder::new
-			@builder.add_from_file("Connect4.glade")
+			@builder.add_from_file("Connect4_new.glade")
 			@builder.connect_signals{ |handler| method(handler) } 
 	
 			# Quit Gtk when window closed.
@@ -59,13 +62,16 @@ class GUIViewImpl
 			# Set up signals for button clicks
 			0.upto(5) { |i|
 				0.upto(6) { |j|
-					button = @builder.get_object("buttoni" + i.to_s + "j" +j.to_s)
-					button.signal_connect("clicked"){button_clicked(j)}
+					@buttons[i][j] = @builder.get_object("buttoni" + i.to_s + "j" +j.to_s)
+					@buttons[i][j].signal_connect("clicked"){button_clicked(j)}
 				}
 			}
-			@display = @builder.get_object("label1")
+			@client_display = @builder.get_object("label1")
+			@view_displays = []
+			2.upto(4) { |i|
+				@view_displays << @builder.get_object("label" + i.to_s)
+			}
 			@controllers = nil
-			# This will cause the first game type to be checked in the GUI
 			
 			set_up_board
 	
@@ -76,45 +82,50 @@ class GUIViewImpl
 	end
   
 	def clear_board
-		color = Gdk::Color.parse(@@white)
-		0.upto(5) { |i|
-			0.upto(6) { |j|
-				button = @builder.get_object("buttoni" + i.to_s + "j" +j.to_s)
-				button.set_label("")
-				button.modify_bg(Gtk::STATE_NORMAL, color)
-				button.modify_fg(Gtk::STATE_NORMAL, color)
-			}
+		@buttons.each{ |button|
+			button.set_label("")
+			button.modify_bg(Gtk::STATE_NORMAL, @@white)
 		}
 	end
 
 
 	def set_up_board(game_options={})
 		clear_board
-		#TODO Properly integrate
-		game_options[:view] = self
-		@controllers = GameManagerImpl.start_game(game_options)
 		@current_game_type = @next_game_type
-		@display.set_label("New #{@@game_types[@current_game_type]} game. Player 1's turn.")
+		if @next_game_type == 2
+			game_options[:otto_and_toot] = true
+		end
+		@view = get_view(game_options)
+		@controllers = GameManagerImpl.start_game(game_options, @view)
+		@current_game_type = @next_game_type
+		@client_display.set_label("#{@@game_types[@current_game_type]} game.")
+	end
+	
+	def get_view(options)
+		if options[:single_player]
+			if options[:otto_and_toot]
+				GUIOttoAndTootSinglePlayerViewImpl.new(@buttons, @view_displays, 1)
+			else
+				GUIConnect4SinglePlayerViewImpl.new(@buttons, @view_displays, 1)
+			end
+		else
+			if options[:otto_and_toot]
+				GUIOttoAndTootMultiplayerViewImpl.new(@buttons, @view_displays)
+			else
+				GUIConnect4MultiplayerViewImpl.new(@buttons, @view_displays)
+			end
+		end
 	end
 
 	def button_clicked(column)
-		error_message = nil
 		if !GameManagerImpl.game_in_progress
-			@display.set_label("There is no game in progress!")
+			@client_display.set_label("There is no game in progress!")
 		elsif !my_turn
-			error_message = "It's not your turn!"
+			@client_display.set_label("It's not your turn!")
 		elsif !current_controller.game.valid_move(column)
-			error_message = "You can't play there!"
+			@client_display.set_label("You can't play there!")
 		else
 			current_controller.play(column)
-		end
-		
-		unless error_message.nil?
-			if !my_turn
-				@display.set_label("#{error_message} AI's turn.")
-			else
-				@display.set_label("#{error_message} Player #{current_controller.player_number}'s turn.")
-			end
 		end
 	end
 	
@@ -129,20 +140,23 @@ class GUIViewImpl
 	def load_game
 		#TODO Load constantly fails
 		if !GameManagerImpl.save_file_present
-			@display.set_label("There is not game file to load.")
+			@client_display.set_label("There is no game file to load.")
 		elsif !@controllers = GameManagerImpl.load_game
-			@display.set_label("An error occurred while loading.")
+			@client_display.set_label("An error occurred while loading.")
 		else
-			#TODO Determine game type from save file.
-			@current_game_type = @next_game_type
+			if @controllers[0].game.options[:otto_and_toot]
+				@current_game_type = 2
+			else
+				@current_game_type = 1
+			end
 		end	
 	end
 	
 	def save_game
 		if GameManagerImpl.save_game
-			@display.set_label("Game saved successfully.")
+			@client_display.set_label("Game saved successfully.")
 		else
-			@display.set_label("Error occurred while saving.")
+			@client_display.set_label("Error occurred while saving.")
 		end
 	end
 	
@@ -163,84 +177,6 @@ class GUIViewImpl
 		end
 	end
 	
-	def each_set_button(board)
-		blank = Gdk::Color.parse(@@white)
-		board.each_with_index{ |x,i,j|
-			button = @builder.get_object("buttoni" + i.to_s + "j" +j.to_s)
-			if x.nil?
-				button.set_label("")
-				button.modify_bg(Gtk::STATE_NORMAL, blank)
-				button.modify_fg(Gtk::STATE_NORMAL, blank)
-			else
-				yield button, x
-			end
-		}
-				
-	end
-	
-	def connect4_update(board)
-		red = Gdk::Color.parse(@@red)
-		black = Gdk::Color.parse(@@black)
-		each_set_button(board){ |button, x|
-			if x == 1
-				button.modify_bg(Gtk::STATE_NORMAL, red)
-				button.modify_fg(Gtk::STATE_NORMAL, red)
-			elsif x == 2
-				button.modify_bg(Gtk::STATE_NORMAL, black)
-				button.modify_fg(Gtk::STATE_NORMAL, black)
-			end
-		}
-	end
-	
-	def otto_and_toot_update(board)
-		each_set_button(board){ |button, x|
-			if x == 1
-				button.set_label("O")
-			elsif x == 2
-				button.set_label("T")
-			end
-		}
-	end
-	
-	def turn_update(update)
-		puts "JIMMY"
-		if update[:board]
-			case @current_game_type
-			when 1
-				connect4_update(update[:board])
-			when 2
-				otto_and_toot_update(update[:board])
-			end
-		end
-		
-		if update[:game_over]
-			game_over(update[:winner])
-		elsif update[:current_turn]
-			if my_turn
-				@display.set_label("Player #{current_controller.player_number}'s turn.")
-			else 
-				@display.set_label("AI's turn.")
-			end
-		end
-	end
-	
-	def game_over(winner)
-		case winner
-		when 0
-			@display.set_label("Tie game!")
-		when 1..2
-			ai_wins = true
-			[*@controllers].each { |c|
-				if c.player_number == winner
-					@display.set_label("Player #{c.player_number} wins.")
-					ai_wins = false
-				end
-			}
-			if ai_wins
-				@display.set_label("AI wins.")
-			end
-		end
-	end
 end
 
 
