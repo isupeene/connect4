b1f5daaff965cf5beae498c0fe1ab644bdaaedcd
Isaac Supeene <isupeene@ualberta.ca>
2014-11-26 15:55:33 -0700

    Minor changes to terminology.

--- src/contracts/database_manager_contract.rb
+++ src/contracts/database_manager_contract.rb
@@ -12,7 +12,7 @@ module DatabaseManagerContract
 	
 	def load_game_precondition(id)
 		assert(id.is_a?(Integer), "Games are given integer IDs in the MySQL database.")
-		assert(game_list.map{ |entry| entry.id }.include?(id), "The provided id should exist.")
+		assert(saved_games.map{ |entry| entry.id }.include?(id), "The provided id should exist.")
 	end
 	
 	def load_game_postcondition(id, result)
@@ -20,13 +20,13 @@ module DatabaseManagerContract
 			assert(result.is_a?(GameContract), "A game is a game.")
 		else
 			assert(
-				!game_list.map{ |entry| entry.id }.include?(id),
+				!saved_games.map{ |entry| entry.id }.include?(id),
 				"If no game is returned, it means it was deleted before we could access it."
 			)
 		end
 	end
 	
-	def game_list_postcondition(result)
+	def saved_games_postcondition(result)
 		assert(
 			result.is_a?(Enumerable) && result.all?{ |game| game.is_a?(GameContract) },
 			"Every game is a game!"
