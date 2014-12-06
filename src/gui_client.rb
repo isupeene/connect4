35e42ab3e2df745eeb866614e45f41b784032873
bsoetaer <bsoetaer@ualberta.ca>
2014-12-05 20:10:49 -0800

    Looks like CLI, now TESTING YAY!

--- src/gui_client.rb
+++ src/gui_client.rb
@@ -1,4 +1,3 @@
-
 require 'gtk2'
 require_relative "about_dialog"
 require_relative "load_dialog"
@@ -10,6 +9,14 @@ require_relative 'gui_connect4_single_player_view'
 require_relative 'gui_connect4_multiplayer_view'
 require_relative 'gui_otto_and_toot_single_player_view'
 require_relative 'gui_otto_and_toot_multiplayer_view'
+require_relative 'implementation/gui_view_server_impl' # TODO: no impl
+require_relative 'implementation/remote_controller_impl'
+require_relative 'implementation/remote_master_server_impl'
+require_relative 'implementation/remote_game_manager_impl'
+
+require 'xmlrpc/client'
+require 'xmlrpc/server'
+require 'socket'
 
 # Client to play connect 4 or otto and toot through a graphical user interface.
 class GUIClient
@@ -28,7 +35,7 @@ class GUIClient
 	
 			# Quit Gtk when window closed.
 			window = @builder.get_object("window1")
-			window.signal_connect( "destroy" ) { Gtk.main_quit }
+			window.signal_connect( "destroy" ) { exit_program }
 	      	
 			# Set up for new single player game
 			menu_option = @builder.get_object("imagemenuitem1")
@@ -37,7 +44,15 @@ class GUIClient
 			# Set up for new 2 player game
 			menu_option = @builder.get_object("imagemenuitem2")
 			menu_option.signal_connect( "activate" ) { set_up_board }
-	      	
+				
+			# Set up for joining server 2 player game
+			menu_option = @builder.get_object("joinmenuitem")
+			menu_option.signal_connect( "activate" ) { join_server }
+			
+			# Set up for leaving server 2 player game
+			menu_option = @builder.get_object("leavemenuitem")
+			menu_option.signal_connect( "activate" ) { leave_server }
+
 			# Set up Open in menu to load game
 			menu_option = @builder.get_object("imagemenuitem3")
 			menu_option.signal_connect( "activate" ) { load_game }
@@ -52,11 +67,15 @@ class GUIClient
 	
 			# Set up Quit menu option
 			menu_option = @builder.get_object("imagemenuitem6")
-			menu_option.signal_connect( "activate" ) { Gtk.main_quit }
+			menu_option.signal_connect( "activate" ) { exit_program }
 				
 			# Set up Player name change menu option
 			menu_option = @builder.get_object("namemenuitem")
 			menu_option.signal_connect( "activate" ) { name_entry }
+				
+			# Set up for changing master server
+			menu_option = @builder.get_object("changemastermenuitem")
+			menu_option.signal_connect( "activate" ) { set_master }
 				
 			# Set up About menu option
 			menu_option = @builder.get_object("aboutmenuitem")
@@ -83,6 +102,8 @@ class GUIClient
 			@leaderboard_dialog = LeaderboardDialog.new
 			@load_dialog = LoadDialog.new
 			@name_dialog = NameDialog.new
+			@join_dialog = JoinDialog.new
+			@change_master_dialog = ChangeMasterDialog.new
 			@player_names = ["","Player 2"]
 			name_entry
 	      	
@@ -103,15 +124,110 @@ class GUIClient
 			}
 			@controllers = nil
 			@game_manager = GameManager.new
+			@master_server = nil
+			set_master("localhost") # TEMP: for testing
+			@remote_view = GUIViewServerImpl.new(@buttons, @view_displays)
 			
-			set_up_board
+			@port = 50530
+			begin
+				@view_server = XMLRPC::Server.new(@port)
+				@view_server.add_handler("view", @remote_view)
+				@view_server.add_handler("client", self)
+				Thread.new{ @view_server.serve }
+			rescue Errno::EADDRINUSE
+				@port += 1
+				if @port < 50550
+					retry
+				else
+					@message_dialog.run(
+						"Ports 50530 to 50549 are busy.  Online play will be disabled for this session."
+					)
+					@view_server = nil
+				end
+			end
+		
 	
 			window.show()
-			Gtk.main()
-			
+			begin
+				Gtk.main()
+			ensure
+				leave_server if connected_to_game_server
+			end
 		end
 	end
-  
+	
+	def exit_program
+		leave_server if @game_server
+		@view_server.shutdown if @view_server
+		Gtk.main_quit
+	end
+	
+	def connected_to_game_server
+		# Kind of a hack? Maybe check for GameServerContract instead.
+		@game_manager.is_a?(RemoteGameManagerImpl)
+ 	end
+	
+	def join_server 
+		if_connected_to_master{
+			server_list = @master_server.server_list
+			server_number = @join_dialog.run(server_list)
+			server = server_list.find{ |s| s["id"] == server_number }
+			if !server
+				@message_dialog.puts("This server was not found.")
+			elsif server["number_of_players"] == 2
+				@message_dialog.puts("This server is already full.")
+			else 
+				# TODO: fix race condition (check if join succeeds)
+				leave_server if connected_to_game_server
+				@game_manager = RemoteGameManagerImpl.new(server)
+				@game_manager.join(get_player_object)
+			end
+		}
+	end
+	
+	def leave_server
+		if_connected_to_game_server{
+			@game_manager.leave(get_player_object)
+			@game_manager = GameManager.new
+		}
+	end
+	
+	def get_player_object
+		{
+			# TODO: correct values
+			"username" => "isupeene",
+			"port" => @port,
+			"hostname" => "localhost"
+		}
+	end
+	
+	def set_master
+		if_online{
+			hostname = @change_master_dialog.run
+			unless hostname.empty?
+				@master_server = RemoteMasterServerImpl.new({
+					"hostname" => hostname,
+					"port" => 50550
+				})
+			end
+		}
+	end
+	
+	def end_game
+		@game_manager.end_game
+	end
+	
+	def open_server
+		connection_info = @master_server.open_server(get_player_object)
+		if connection_info
+			leave_server if @game_server
+			# TODO: No localhost
+			@game_manager = RemoteGameManagerImpl.new(connection_info)
+		elsif
+			@message_dialog.puts("Attempt to open server failed.")
+		end
+	end
+	
 	# Clear board back to its original state, all blank, white buttons
 	def clear_board
 		@buttons.each{ |row|
@@ -121,20 +237,108 @@ class GUIClient
 			}
 		}
 	end
+	
+	def if_online
+		if @view_server
+			yield
+		else
+			@message_dialog.puts("Not available in offline mode.")
+		end
+	end
+	
+	# Perform block if a game is in progress.
+ 	def if_game_in_progress
+		if game_in_progress
+ 			yield
+ 		else
+			@message_dialog.puts("There's currently no game in progress!")
+ 		end
+ 	end
+ 	
+ 	def game_in_progress
+		@game_manager.game_in_progress
+ 	end
+ 	
+ 	def server_owner
+		# Every place where we check the type of game manager is kind of a hack.
+		!connected_to_game_server || @game_manager.server_owner(get_player_object)
+	end
+	
+	def if_server_owner
+		if_connected_to_game_server{
+			if server_owner
+				yield
+			else
+				@out.puts("Only the server owner can do that!")
+			end
+		}
+	end
+	
+	def if_connected_to_master
+		if @master_server
+			yield
+		else
+			@out.puts("You're not connected to the master server!")
+		end
+	end
+	
+	def if_connected_to_game_server
+		if connected_to_game_server
+			yield
+		else
+			@out.puts("You're not connected to a game server!")
+		end
+	end
 
 	# Set up board based on game options and game type.
 	# Create an appropriate view for the game 
-	def set_up_board(game_options={})
+	def set_up_board(options={})
+		end_game if game_in_progress
 		clear_board
 		@current_game_type = @next_game_type
 		if @next_game_type == 2
-			game_options["otto_and_toot"] = true
+			options["otto_and_toot"] = true
+			@remote_view.set_otto_and_toot
+		else
+			@remote_view.set_connect4
 		end
 		
-		@controllers = @game_manager.start_game(game_options, get_view(game_options))
-		@current_game_type = @next_game_type
+		# Start new server if multiplayer game
+		if !options["single_player"]
+			if_connected_to_master{ open_server }
+		end
+		
+		if connected_to_game_server
+			if_server_owner {
+				@remote_view.set_player_number(1)
+				@controllers = @game_manager.start_game(options)
+			}
+		else
+			@controllers = @game_manager.start_game(options, get_view(options))
+
+		end
+		
 		@client_display.set_label("#{@@game_types[@current_game_type]} game.")
 	end
+	
+	def starting_remote_game(game_options, controller)
+		begin
+			if game_options["otto_and_toot"]
+				@current_game_type = 2
+				@remote_view.set_otto_and_toot
+			else
+				@current_game_type = 1
+				@remote_view.set_connect4
+			end
+			@remote_view.set_player_number(2)
+			@controllers = RemoteControllerImpl.new(controller)
+		rescue Exception => ex
+			puts ex.message
+			puts ex.backtrace
+			raise ex
+		end
+		return true
+ 	end
 	
 	# Returns an appropriate view for the game based on the options.
 	def get_view(options)
@@ -155,11 +359,11 @@ class GUIClient
 
 	# Validates move and plays the move if it is valid.
 	def button_clicked(column)
-		if !@game_manager.game_in_progress
+		if !game_in_progress
 			@client_display.set_label("There is no game in progress!")
 		elsif !my_turn
 			@client_display.set_label("It's not your turn!")
-		elsif !current_controller.game.valid_move(column)
+		elsif !current_controller.valid_move(column)
 			@client_display.set_label("You can't play there!")
 		else
 			current_controller.play(column)
@@ -188,8 +392,10 @@ class GUIClient
 	
 	# TODO interface with backend. Get different stats for different game types.
 	def leaderboard(chosen_type)
-		rankings = [Stat.new("Bob"), Stat.new("Frank")]
-		@leaderboard_dialog.run(@@game_types[chosen_type], rankings)
+		if_connected_to_master{
+			rankings = @master_server.leaderboards(chosen_type)
+			@leaderboard_dialog.run(@@game_types[chosen_type], rankings)
+		}
 	end
 	
 	# Load game into client
@@ -251,3 +457,4 @@ class GUIClient
 end
 
 GUIClient.new
+
